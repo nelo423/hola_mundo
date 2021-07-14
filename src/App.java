84ed9937dea9365138cd0178a13d64e612760ab4
nelo423 <nelo23419@hotmail.com>
2021-07-13 19:32:04 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        claseOperacion operacion = new claseOperacion();
+        System.out.println(operacion.r);
+    }
+}
---
+++ src/claseOperacion.java
@@ -0,0 +1,6 @@
+public class claseOperacion {
+   int numeroUno = 5;
+   int numeroDos = 5;
+   int r = numeroUno + numeroDos;
+
+}
